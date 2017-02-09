0120856f2fbc7ab50adae291372d8d7d1e47fc35
CraigRodrigues <craig@craigrodrigues.com>
2017-02-09 10:47:50 -0800

    Updated removeNode to also remove edges when removing a node. Completed forEachNode and passed all tests for graph.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -22,6 +22,11 @@ Graph.prototype.removeNode = function(node) {
   if (index > - 1) {
     this.vertices.splice(index, 1);
   }
+  for (var i = 0; i < this.edges.length; i++) {
+    if ((this.edges[i][0] === node || this.edges[i][1] === node)) {
+      this.edges.splice(i, 1);
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -43,10 +48,17 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  for (var i = 0; i < this.edges.length; i++) {
+    if ((this.edges[i][0] === fromNode && this.edges[i][1] === toNode) || 
+        (this.edges[i][0] === toNode && this.edges[i][1] === fromNode)) {
+      this.edges.splice(i, 1);
+    }
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  _.each(this.vertices, cb);
 };
 
 /*
