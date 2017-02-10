93df18c89bb0b1c76836ddf0b5365daec80becd7
CraigRodrigues <craig@craigrodrigues.com>
2017-02-09 16:02:27 -0800

    Changed hash table to use objects instead of arrays for conflict resolution. Passed all tests.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,17 +7,29 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  var value = this._storage.get(index);
+  var obj = {};
+  obj[k] = v;
+  if (value === undefined) {
+    this._storage.set(index, obj);
+  } else {
+    this._storage.set(index, _.extend(value, obj));
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var value = this._storage.get(index);
+
+  if (value.hasOwnProperty(k)) {
+    return value[k];
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+  var value = this._storage.get(index);
+  delete value[k];
 };
 
 
